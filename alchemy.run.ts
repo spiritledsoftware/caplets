e19a1400fd7992122dab6fcc528db39d6e25b99d
Ian Pascoe <ian.g.pascoe@gmail.com>
2026-05-28 13:53:25 -0400

    ci: fix preview deploys, teardown, add www. domain

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -3,14 +3,29 @@ import { Astro } from "alchemy/cloudflare";
 import { GitHubComment } from "alchemy/github";
 import { CloudflareStateStore } from "alchemy/state";
 
-const baseDomain = "caplets.dev";
+const globalBaseDomain = "caplets.dev";
 
 const app = await alchemy("caplets", {
   stateStore: (scope) => new CloudflareStateStore(scope),
   password: process.env.ALCHEMY_PASSWORD!,
 });
 
-const landingPageDomain = app.stage === "prod" ? baseDomain : `${app.stage}.preview.${baseDomain}`;
+const baseDomain =
+  app.stage === "prod" ? globalBaseDomain : `${app.stage}.preview.${globalBaseDomain}`;
+
+const landingPageDomain = baseDomain;
+export const landingPage = await Astro("landing-page", {
+  cwd: "apps/landing",
+  dev: {
+    command: "pnpm run dev" + (process.env.SSH_CONNECTION ? " --host 0.0.0.0" : ""),
+  },
+  domains: [landingPageDomain, `www.${landingPageDomain}`],
+});
+
+console.log({
+  "Landing Page URL": `https://${landingPageDomain}`,
+});
+
 const [repositoryOwnerFromSlug, repositoryNameFromSlug] =
   process.env.GITHUB_REPOSITORY?.split("/") ?? [];
 const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER ?? repositoryOwnerFromSlug;
@@ -18,19 +33,6 @@ const repositoryName = process.env.GITHUB_REPOSITORY_NAME ?? repositoryNameFromS
 const pullRequestNumber = process.env.PULL_REQUEST
   ? Number.parseInt(process.env.PULL_REQUEST, 10)
   : undefined;
-
-export const landingPage = await Astro("landing-page", {
-  cwd: "apps/landing",
-  dev: {
-    command: "pnpm run dev" + (process.env.SSH_CONNECTION ? " --host 0.0.0.0" : ""),
-  },
-  domains: [landingPageDomain],
-});
-
-console.log({
-  "Landing Page URL": landingPage.url,
-});
-
 if (pullRequestNumber) {
   if (!repositoryOwner || !repositoryName) {
     throw new Error("Missing GitHub repository metadata for preview comment.");
